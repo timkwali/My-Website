27eb518fc264ae8eca71763d027b5e0b800675bb
Timring <timringperez@gmail.com>
2020-01-07 08:16:04 +0100

    completed navigation bar

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+  function scroll_to(clicked_link, nav_height) {
+    var element_class = clicked_link.attr("href").replace("#", ".");
+    var scroll_to = 0;
+    if(element_class != ".home") {
+      element_class += ".-container";
+      scroll_to = $(element_class).offset().top - nav_height;
+    }
+    if($(window).scrollTop() != scroll_to) {
+      $("html, body").stop().animate({scrollTop: scroll_to}, 1000);
+    }
+  }
+
+  // toggle "navbar-no-bg" class
+  $('.top-content .text').waypoint(function() {
+    $('nav').toggleClass('navbar-no-bg');
+  });
+
+  //navigation
+  $("a.scroll-link").on("click", function(e) {
+    e.preventDefault();
+    scroll_to($(this), $("nav").outerHeight());
+  });
+
+  
+});
