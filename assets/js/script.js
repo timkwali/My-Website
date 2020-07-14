cce9157a00caa7870f9abab38dd1f21efcdaf9fb
Timring <timringperez@gmail.com>
2020-07-14 18:50:04 +0100

    add files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
     var element_class = clicked_link.attr("href");
     var scroll_to = 0;
     if(element_class != "#home") {
-      scroll_to = $(element_class).offset().top - nav_height;
+      scroll_to = $(element_class).offset().top;
     }
     if($(window).scrollTop() != scroll_to) {
       $("html, body").stop().animate({scrollTop: scroll_to}, 1000);
